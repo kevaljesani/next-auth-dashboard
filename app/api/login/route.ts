96203848797270c9a69e9cae13389cce57d668a9
coder <65643094+kevaljesani@users.noreply.github.com>
2026-05-01 00:15:26 +0530

    Update route.ts

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,17 +1,49 @@
-// app/api/login/route.ts
 import { NextResponse } from "next/server";
+import User from "../../model/user";
+import DBConnection from "../../lib/db";
 
 export async function POST(req: Request) {
-  const { email, password } = await req.json();
+  try {
+    await DBConnection();
 
-  if (email === "admin@test.com" && password === "123456") {
-    return NextResponse.json({
-      token: "mock-token-123",
-    });
+    const body = await req.json();
+
+    // console.log("BODY:", body);
+
+    const email = body.email?.trim().toLowerCase();
+    const password = body.password;
+
+    const user = await User.findOne({ email });
+
+    // console.log("FOUND USER:", user);
+
+    if (!user) {
+      return NextResponse.json(
+        { message: "User not found" },
+        { status: 404 }
+      );
+    }
+
+    if (user.password !== password) {
+      return NextResponse.json(
+        { message: "Invalid password" },
+        { status: 401 }
+      );
+    }
+return NextResponse.json({
+  message: "Login success",
+  user: {
+    id: user._id,
+    email: user.email,
+    name: user.name,
+  },
+});
+
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { message: "Server error" },
+      { status: 500 }
+    );
   }
-
-  return NextResponse.json(
-    { message: "Invalid credentials" },
-    { status: 401 }
-  );
 }
